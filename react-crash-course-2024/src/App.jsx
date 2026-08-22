cddcaf4696d87c66b4abc87fd3752ef86f08b94c
unknown <durgeshkhandelwal2019@gmail.com>
2026-08-22 18:12:59 +0530

    Add: Learning Components - View All Jobs

--- react-crash-course-2024/src/App.jsx
+++ react-crash-course-2024/src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import HomeCards from './components/HomeCards';
 import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 const App = () => {
   return (
@@ -16,13 +17,7 @@ const App = () => {
       
       <JobListings />
 
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-          >View All Jobs</a
-        >
-      </section>  
+      <ViewAllJobs />
     </>
   )
 }
