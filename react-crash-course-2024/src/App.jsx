261417a87141c1ddba71abbf04be5577ca9d6531
unknown <durgeshkhandelwal2019@gmail.com>
2026-08-22 19:34:55 +0530

    Refactoring: Removing Node Modules from the root for React Router setup

--- react-crash-course-2024/src/App.jsx
+++ react-crash-course-2024/src/App.jsx
@@ -1,25 +1,36 @@
 // import React from 'react'
-import Hero from "./components/Hero"
-import Navbar from "./components/Navbar"
-import HomeCards from './components/HomeCards';
-import JobListings from "./components/JobListings";
-import ViewAllJobs from "./components/ViewAllJobs";
+// import Hero from "./components/Hero"
+// import Navbar from "./components/Navbar"
+// import HomeCards from './components/HomeCards';
+// import JobListings from "./components/JobListings";
+// import ViewAllJobs from "./components/ViewAllJobs";
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
 
+// create our router
+// Route -> Route Component, we can actually pass path within route but for now, it's only for home page, so we can add index and then an element representing the route.
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<h1>"My Routing App"</h1>} />)
+);
+
+// Provider is the one providing the router...
 const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
+
+{/* const App = () => {
   return (
     <>
       <Navbar />
 
-      {/* <Hero /> */}
-      <Hero title='React Dev Testing' subtitle='React Dev passing subtitle via props...'/>
+      <Hero title='React Dev Testing' subtitle='React Dev passing subtitle via props...' />
 
       <HomeCards />
-      
+
       <JobListings />
 
       <ViewAllJobs />
     </>
   )
-}
-
-export default App
+} */}
