873ed33bc006d216d088dc0619198b37bec7ad94
unknown <durgeshkhandelwal2019@gmail.com>
2026-08-22 16:22:44 +0530

    Add: Learning Wrapper Components - Developer and Employers

--- react-crash-course-2024/src/components/HomeCards.jsx
+++ react-crash-course-2024/src/components/HomeCards.jsx
@@ -1,10 +1,13 @@
+import Card from "./Card";
+ 
 const HomeCards = () => {
   return (
     // {/* <!-- Developers and Employers --> */}
     <section className="py-4">
         <div className="container-xl lg:container m-auto">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-            <div className="bg-gray-100 p-6 rounded-lg shadow-md">
+            <Card>
+                {/* <div className="bg-gray-100 p-6 rounded-lg shadow-md"> */}
                 <h2 className="text-2xl font-bold">For Developers</h2>
                 <p className="mt-2 mb-4">
                 Browse our React jobs and start your career today
@@ -15,8 +18,10 @@ const HomeCards = () => {
                 >
                 Browse Jobs
                 </a>
-            </div>
-            <div className="bg-indigo-100 p-6 rounded-lg shadow-md">
+                {/* </div> */}
+            </Card>
+            <Card bg='bg-indigo-100'>
+            {/* <div className="bg-indigo-100 p-6 rounded-lg shadow-md"> */}
                 <h2 className="text-2xl font-bold">For Employers</h2>
                 <p className="mt-2 mb-4">
                 List your job to find the perfect developer for the role
@@ -27,7 +32,8 @@ const HomeCards = () => {
                 >
                 Add Job
                 </a>
-            </div>
+            {/* </div> */}
+            </Card>
             </div>
         </div>
     </section>
