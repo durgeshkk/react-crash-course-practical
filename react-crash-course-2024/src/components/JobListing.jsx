4decf0c7652bd30b3046d73444ecdd7948b15bb7
unknown <durgeshkhandelwal2019@gmail.com>
2026-08-23 10:56:40 +0530

    Full Pages: Anchor Tag -> Link Component

--- react-crash-course-2024/src/components/JobListing.jsx
+++ react-crash-course-2024/src/components/JobListing.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { FaMapMarker } from "react-icons/fa"; // fa = font awesome
+import { Link } from "react-router-dom";
 
 const JobListing = ({job}) => {
   
@@ -39,13 +40,13 @@ const JobListing = ({job}) => {
             <FaMapMarker className="inline text-lg mb-1 mr-1" />
             {job.location}
             </div>
-            <a
+            <Link
             // Redirect this to correct job id
-            href={`/job/${job.id}`}
+            to={`/job/${job.id}`}
             className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
             >
             Read More
-            </a>
+            </Link>
         </div>
         </div>
     </div>
