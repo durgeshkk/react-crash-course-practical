42424f507d6522b512eecf002a8fdc1801d9bbaf
unknown <durgeshkhandelwal2019@gmail.com>
2026-08-23 11:08:21 +0530

    Custom 404 Error Handling

--- react-crash-course-2024/src/App.jsx
+++ react-crash-course-2024/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import HomePage from "./pages/HomePage";
 import MainLayout from './layouts/MainLayout';
 import JobsPage from "./pages/JobsPage";
+import NotFoundPage from './pages/NotFoundPage';
 
 // create our router
 // Route -> Route Component, we can actually pass path within route but for now, it's only for home page, so we can add index and then an element representing the route.
@@ -12,6 +13,8 @@ const router = createBrowserRouter(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
+      {/* Below line is like catch job! */}
+      <Route path="*" element={<NotFoundPage />} /> 
     </Route>
   )
 );
