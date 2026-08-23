83aa8a9b280543ac774ac743b0751d1f8ccb2bad
unknown <durgeshkhandelwal2019@gmail.com>
2026-08-23 19:26:53 +0530

    Delete Job Impl - w/o API Integration

--- react-crash-course-2024/src/App.jsx
+++ react-crash-course-2024/src/App.jsx
@@ -12,6 +12,7 @@ import AddJobPage from "./pages/AddJobPage";
 // Route -> Route Component, we can actually pass path within route but for now, it's only for home page, so we can add index and then an element representing the route.
 // Provider is the one providing the router...
 const App = () => {
+  // Add new Job
   const addJob = async (newJob) => {
     // console.log('App - ',newJob); 
     const res = await fetch('/api/jobs', {
@@ -24,13 +25,26 @@ const App = () => {
     return;
   }
 
+  // Delete Job
+  const deleteJob = async (id) => {
+    console.log('App delete - ',id); 
+    // const res = await fetch('/api/jobs', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify(newJob),
+    // });
+    return;
+  }
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/jobs" element={<JobsPage />} />
         <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
-        <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader}/>
+        <Route path="/jobs/:id" element={<JobPage deleteJob={deleteJob}/>} loader={jobLoader}/>
         {/* Below line is like catch job! */}
         <Route path="*" element={<NotFoundPage />} /> 
       </Route>
--- react-crash-course-2024/src/pages/JobPage.jsx
+++ react-crash-course-2024/src/pages/JobPage.jsx
@@ -1,8 +1,18 @@
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
-import { Link, useLoaderData } from "react-router-dom";
+import { Link, useNavigate, useLoaderData } from "react-router-dom";
 
-const JobPage = () => {
+const JobPage = ({ deleteJob }) => {
     const job = useLoaderData();
+    const navigate = useNavigate();
+
+    const onDeleteClick = (jobId) => {
+        const confirm = window.confirm('Are you sure you want to delete this listing?')
+
+        if(!confirm) return;
+
+        deleteJob(jobId);
+        navigate('/jobs');
+    }
 
     return (
         <>
@@ -86,7 +96,7 @@ const JobPage = () => {
                         className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                         >Edit Job</Link
                     >
-                    <button
+                    <button onClick={() => onDeleteClick(job.id)}
                         className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                     >
                         Delete Job
