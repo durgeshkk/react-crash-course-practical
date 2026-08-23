91e4f41d5b774be2ff7a6e537b7cf3303a7b5def
unknown <durgeshkhandelwal2019@gmail.com>
2026-08-23 18:53:47 +0530

    Navigate prop, prop from parent to child and child to parent triggers fxn call

--- react-crash-course-2024/src/App.jsx
+++ react-crash-course-2024/src/App.jsx
@@ -7,24 +7,36 @@ import NotFoundPage from './pages/NotFoundPage';
 import JobPage, {jobLoader} from "./pages/JobPage";
 import AddJobPage from "./pages/AddJobPage";
 
+
 // create our router
 // Route -> Route Component, we can actually pass path within route but for now, it's only for home page, so we can add index and then an element representing the route.
-const router = createBrowserRouter(
-  // createRoutesFromElements(<Route index element={<h1>"My Routing App"</h1>} />)
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path="/jobs" element={<JobsPage />} />
-      <Route path="/add-job" element={<AddJobPage />} />
-      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader}/>
-      {/* Below line is like catch job! */}
-      <Route path="*" element={<NotFoundPage />} /> 
-    </Route>
-  )
-);
-
 // Provider is the one providing the router...
 const App = () => {
+  const addJob = async (newJob) => {
+    // console.log('App - ',newJob); 
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  }
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader}/>
+        {/* Below line is like catch job! */}
+        <Route path="*" element={<NotFoundPage />} /> 
+      </Route>
+    )
+  );
+
   return <RouterProvider router={router} />;
 };
 
