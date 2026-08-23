369e1e1a50534942f66132611d01f3f3e190b5a1
unknown <durgeshkhandelwal2019@gmail.com>
2026-08-23 13:15:50 +0530

    Loading Spinner - React Spinner and Basic - Pagination

--- react-crash-course-2024/src/components/JobListings.jsx
+++ react-crash-course-2024/src/components/JobListings.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import JobListing from './JobListing'
+import Spinner from './Spinner';
 
 const JobListings = ({ isHome = false }) => {
 
@@ -10,13 +11,18 @@ const JobListings = ({ isHome = false }) => {
     () => {
       // init fetchJobs
       const fetchJobs = async () => {
+        const apiUrl = isHome
+          ? 'http://localhost:8000/jobs?_page=1&_per_page=3'
+          : 'http://localhost:8000/jobs';
+
         try {
-          const res = await fetch('http://localhost:8000/jobs');
+          const res = await fetch(apiUrl);
           const data = await res.json();
-          setJobs(data);
+          setJobs(isHome ? data.data : data);
         } catch(error) {
           console.log('Error fetchin data', error);
         } finally {
+          await new Promise((resolve) => setTimeout(resolve, 2000));
           setLoading(false);
         }
       }
@@ -35,11 +41,17 @@ const JobListings = ({ isHome = false }) => {
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
         {isHome ? 'Recent Jobs' : 'Browse Jobs'}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {jobs.map((job) => (
-                <JobListing key={job.id} job={job}/>
-            ))}
-        </div>
+        {
+          loading ? (
+            <Spinner loading={loading}/>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {jobs.map((job) => (
+                  <JobListing key={job.id} job={job}/>
+              ))}
+            </div>
+          )
+        }
     </div>
     </section>
   )
