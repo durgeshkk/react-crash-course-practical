b9a5cdc58960c96ca249839eb3baef031f5106e7
unknown <durgeshkhandelwal2019@gmail.com>
2026-08-23 12:21:23 +0530

    Setup: JSON Server

--- react-crash-course-2024/src/components/JobListings.jsx
+++ react-crash-course-2024/src/components/JobListings.jsx
@@ -1,9 +1,9 @@
 import jobs from '../jobs.json'
 import JobListing from './JobListing'
 
-const JobListings = () => {
+const JobListings = ({ isHome = false }) => {
 
-  const recentJobs = jobs.slice(0,3);
+  const jobListings = isHome ? jobs.slice(0,3) : jobs;
   
   return (
     // <div>JobListing</div>
@@ -11,11 +11,11 @@ const JobListings = () => {
     <section className="bg-blue-50 px-4 py-10">
     <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-        Browse Jobs
+        {isHome ? 'Recent Jobs' : 'Browse Jobs'}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {/* {jobs.map((job) => ( */}
-            {recentJobs.map((job) => (
+            {jobListings.map((job) => (
                 // Since for all of the jobs listed components, heading, indentation and all will remain same, so grab either of them and paste within...
                 <JobListing key={job.id} job={job}/>
             ))}
