079395d81ac23e2319e7629572a743d38d063c98
unknown <durgeshkhandelwal2019@gmail.com>
2026-08-23 20:13:46 +0530

    Edit Job Page

--- react-crash-course-2024/src/App.jsx
+++ react-crash-course-2024/src/App.jsx
@@ -6,6 +6,7 @@ import JobsPage from "./pages/JobsPage";
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage, {jobLoader} from "./pages/JobPage";
 import AddJobPage from "./pages/AddJobPage";
+import EditJobPage from "./pages/EditJobPage";
 
 
 // create our router
@@ -21,6 +22,19 @@ const App = () => {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(newJob),
+    });
+    return;
+  }
+
+  // Update Job
+  const updateJob = async (job) => {
+    // console.log('App - ',newJob); 
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(job),
     });
     return;
   }
@@ -43,6 +57,7 @@ const App = () => {
         <Route index element={<HomePage />} />
         <Route path="/jobs" element={<JobsPage />} />
         <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route path="/edit-job/:id" element={<EditJobPage updateJobSubmit={updateJob}/>} loader={jobLoader}/>
         <Route path="/jobs/:id" element={<JobPage deleteJob={deleteJob}/>} loader={jobLoader}/>
         {/* Below line is like catch job! */}
         <Route path="*" element={<NotFoundPage />} /> 
