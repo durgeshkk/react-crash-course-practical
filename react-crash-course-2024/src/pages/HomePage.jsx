8aca4876522a15385aa8ea4deea1ee2c4f6f50e6
unknown <durgeshkhandelwal2019@gmail.com>
2026-08-23 10:28:28 +0530

    Add: HomePage Layout

--- react-crash-course-2024/src/pages/HomePage.jsx
+++ react-crash-course-2024/src/pages/HomePage.jsx
@@ -1,11 +1,14 @@
-// import HomeCards from './components/HomeCards';
-// import JobListings from "./components/JobListings";
-// import ViewAllJobs from "./components/ViewAllJobs";
 import Hero from "../components/Hero"
+import HomeCards from '../components/HomeCards';
+import JobListings from "../components/JobListings";
+import ViewAllJobs from "../components/ViewAllJobs";
 const HomePage = () => {
   return (
     <>
         <Hero />
+        <HomeCards />
+        <JobListings />
+        <ViewAllJobs />
     </>
   )
 }
